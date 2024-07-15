1c06787286c39008c8364c1d09e1412bbc4b7c66
sowmik-sec <sowmik.sec@gmail.com>
2024-07-15 10:51:52 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <>
+      <h1>Users Management System</h1>
+      <p>Number of users: {users.length}</p>
+    </>
+  );
+}
+
+export default App;
