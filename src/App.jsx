488b55ccd4859ab61fcfe38d929c0a5a69d16614
sowmik-sec <sowmik.sec@gmail.com>
2024-07-15 11:12:07 +0600

    send data to server by post request

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,45 @@ function App() {
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = { name, email };
+    console.log(name, email, user);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUsers([...users, data]);
+        form.reset();
+      });
+  };
   return (
     <>
       <h1>Users Management System</h1>
-      <p>Number of users: {users.length}</p>
+      <h3>Number of users: {users.length}</h3>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.id} {user.name} : {user.email}
+          </p>
+        ))}
+      </div>
     </>
   );
 }
